45721616fcb67c3bfbc0133cfd90aeee2a4ee8f5
softlocker <softlockerin@gmail.com>
2025-01-15 14:34:50 +0530

    first commit

--- lib/authentication/login1.dart
+++ lib/authentication/login1.dart
@@ -5,12 +5,35 @@ class loginscreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-            gradient: LinearGradient(colors: [
-          Color(0xFFFFFF).withOpacity(1.0),
-          Color(0x999999).withOpacity(1.0)
-        ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 30),
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [
+              Color(0xFFFFFF).withOpacity(1.0),
+              Color(0x999999).withOpacity(1.0)
+            ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              width: double.infinity,
+              height: 620,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [
+                  Color(0x92E1E2).withOpacity(1.0),
+                  Color(0x0D4671).withOpacity(1.0)
+                ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(190),
+                    bottomRight: Radius.circular(190)),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
